969d1134e756802ae53870a70b2e13f60b89e011
guenthner <guenthner.jonathan@gmail.com>
2025-02-28 18:42:45 +0100

    Added clang-format

--- recaman.c
+++ recaman.c
@@ -1,13 +1,13 @@
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 typedef long long number;
 
 typedef struct {
-    number current_term;
-    number bound;
-    char* bitset;
-    int extra_state;
+	number current_term;
+	number bound;
+	char *bitset;
+	int extra_state;
 } RecamanState;
 
 RecamanState createState(number bound) {
@@ -19,61 +19,60 @@ RecamanState createState(number bound) {
 	return state;
 }
 
-void destroyState(RecamanState* state) {
-    free(state->bitset);
-    state->bitset = 0;
+void destroyState(RecamanState *state) {
+	free(state->bitset);
+	state->bitset = 0;
 }
 
-void out_of_bounds(RecamanState* state, number num) {
-    destroyState(state);
-    exit(0);
+void out_of_bounds(RecamanState *state, number num) {
+	destroyState(state);
+	exit(0);
 }
 
-char number_hit(RecamanState* state, number num) {
-    if (num >= state->bound) {
-        return 0;
-    }
+char number_hit(RecamanState *state, number num) {
+	if (num >= state->bound) {
+		return 0;
+	}
 
-   return (state->bitset[num >> 3] >> (num & 0b111)) & 1;
+	return (state->bitset[num >> 3] >> (num & 0b111)) & 1;
 }
 
-void hit_number(RecamanState* state, number num) {
-    if (num >= state->bound) {
-        out_of_bounds(state, num);
-        return;
-    }
+void hit_number(RecamanState *state, number num) {
+	if (num >= state->bound) {
+		out_of_bounds(state, num);
+		return;
+	}
 
-    state->bitset[num >> 3] |= (1 << (num & 0b111));
+	state->bitset[num >> 3] |= (1 << (num & 0b111));
 }
 
 void print_help() {
-    printf("Usage: recaman <MODE> <BOUND>\n");
-    printf("\n");
-    printf("This is the recaman tool for working with the recaman sequence (https://oeis.org/A005132)\n");
-    printf("\n");
-    printf("The possible modes are:\n");
-    printf("\t0: Generate the values of the recaman sequence\n");
+	printf("Usage: recaman <MODE> <BOUND>\n");
+	printf("\n");
+	printf("This is the recaman tool for working with the recaman sequence "
+	       "(https://oeis.org/A005132)\n");
+	printf("\n");
+	printf("The possible modes are:\n");
+	printf("\t0: Generate the values of the recaman sequence\n");
 }
 
 void recaman(int mode, number bound) {
-  if (bound & 0b111) {
-    bound = (((bound >> 3) + 1) << 3);
-  }
+	if (bound & 0b111) {
+		bound = (((bound >> 3) + 1) << 3);
+	}
 
-  RecamanState state = createState(bound);
+	RecamanState state = createState(bound);
 
-   
-
-  destroyState(&state);
+	destroyState(&state);
 }
 
-int main(int argc, char** args) {
-    if (argc != 3) {
-        print_help();
-        return -1;
-    }
+int main(int argc, char **args) {
+	if (argc != 3) {
+		print_help();
+		return -1;
+	}
 
-    recaman(0, 3);
+	recaman(0, 3);
 
-    return 0;
+	return 0;
 }
